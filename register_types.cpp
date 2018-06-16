48f36e37c78b7e748688d9203ac8e78a1542d528
Jim Breslin <jbreslin33@gmail.com>
2018-06-16 12:05:25 -0400

    rm

--- register_types.cpp
+++ register_types.cpp
@@ -4,8 +4,6 @@
 #include "class_db.h"
 #include "autolist.h"
 #include "CellSpacePartition.h"
-#include "Cgdi.h"
-#include "ConsoleUtils.h"
 #include "FrameCounter.h"
 #include "iniFileLoaderBase.h"
 #include "PriorityQueue.h"
@@ -13,12 +11,10 @@
 #include "Stream_Utility_Functions.h"
 #include "TypeToString.h"
 #include "utils.h"
-#include "WindowUtils.h"
 
 void register_breslinmisc_types() {
 
         ClassDB::register_class<CellSpacePartition>();
-        ClassDB::register_class<Cgdi>();
         ClassDB::register_class<FrameCounter>();
         ClassDB::register_class<iniFileLoaderBase>();
         ClassDB::register_class<Smoother>();
